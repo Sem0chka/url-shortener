ebe41d9522c5402ef48db95c2d2a165a23c2dcdc
Sem0chka <me.peremy@gmail.com>
2024-01-16 22:51:51 +0300

    update files

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -4,6 +4,8 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage/sqlite"
 )
 
 const (
@@ -17,9 +19,18 @@ func main() {
 
 	log := setupLogger(cfg.Env)
 
-	log.Info("starting url-shortener")
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 }
+
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 	switch env {
