abf4bf6d677df9dcbef836fae0b5466c0d53113f
artforme <me.peremy@gmail.com>
2024-01-19 21:36:39 +0300

    feat: add authorization

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -46,7 +46,14 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
-	router.Post("/url", save.NEW(log, storage))
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+
+		r.Post("/", save.NEW(log, storage))
+	})
+
 	router.Get("/{alias}", redirect.New(log, storage))
 
 	log.Info("starting server", slog.String("adress", cfg.Address))
