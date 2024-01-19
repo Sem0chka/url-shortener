2f6d3b56d5d94c234f2ab05a0622cd854a97beb7
artforme <me.peremy@gmail.com>
2024-01-19 16:06:14 +0300

    Create new file that returns random alias

---
+++ internal/lib/random/random.go
@@ -0,0 +1,27 @@
+package random
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	end   = 122
+	start = 97
+)
+
+// NewRandomString generates random string with given size.
+func NewRandomString(size int) string {
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		"0123456789")
+
+	b := make([]rune, size)
+	for i := range b {
+		b[i] = chars[rnd.Intn(len(chars))]
+	}
+
+	return string(b)
+}
